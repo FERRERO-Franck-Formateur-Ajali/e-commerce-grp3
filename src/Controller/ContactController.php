d22bf5e540220938352678b3f24275a16ba88e0c
Larsenik67 <ylachqar@gmail.com>
2021-07-08 11:23:01 +0200

    Init

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -17,10 +17,21 @@ class ContactController extends AbstractController
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
-        $entityManager = $this->getDoctrine()->getManager();
-        if ($form->isSubmitted()){
-            $entityManager->persist($contact);
-            $entityManager->flush();
+        #$entityManager = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() and $form->isValid()){
+            $to = "ylachqar@gmail.com"; // this is your Email address
+            $from = $_POST['email']; // this is the sender's Email address
+            $name = $_POST['nom'];
+            $subject = $_POST['objet'];
+            $subject2 = "Copie de votre message sur Cash Zone";
+            $message = $name . " vous a écrit le message suivant:" . "\n\n" . $_POST['contenu'];
+            $message2 = "Voici une copie de votre message" . $name . "\n\n" . $_POST['contenu'];
+        
+            $headers = "From:" . $from;
+            $headers2 = "From:" . $to;
+            mail($to,$subject,$message,$headers);
+            mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+            echo "Email envoyé, merci " . $name . ", nous vous répondrons dans les plus brefs délais.";
         }
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
